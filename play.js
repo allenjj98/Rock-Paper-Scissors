4d915ede8984d01004d1dd2bb00776e5641b0f63
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-02-21 09:22:08 +0000

    Create pseudocode for the project

---
+++ play.js
@@ -0,0 +1,24 @@
+// create getComputerChoice function to randomly return either 'Rock', 'Paper' or 'Scissors'
+
+// Assign the return value from getComputerChoice to computerSelection variable. 
+
+// Ask the user to input either 'Rock', 'Paper' or 'Scissors'. Store the value in a playerSelection variable. 
+
+// Standardise player input to ensure that its case-insensitive. 
+
+// create playRound function that has two parameters(computerSelection and playerSelection). 
+    // compare the values to decide the outcome of the game. 
+    // Return a string that declares the outcome of the game.
+
+// create playGame function. 
+    // create playerScore variable and initialise to 0
+    // create computerScore variable and initialise to 0
+    // create roundCount variable and initialise to 5
+    // create keepGoing variable and initialise to True
+
+
+    
+    // While the keepGoing variable is true call the playRound function
+    // depending on the outcome of the game increment either the playerScore or computerScore variable by 1.
+    // After each round decrement the roundCount variable by 1. Once the variable reaches 0 reassign the keepGoing variable to False. 
+    // compare playerScore and computerScore and output the winner to the console. 
