5e08b98cbdbfb100f990dc4464f5203e5a41bde4
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-02-22 10:14:57 +0000

    Create function that randomly returns a string
    Created a function that randomly returns Rock, Paper
    or Scissors. As a result, the computers selection is randomised.

--- play.js
+++ play.js
@@ -1,10 +1,22 @@
 // create getComputerChoice function to randomly return either 'Rock', 'Paper' or 'Scissors'
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    let randomChoice = options[Math.floor(Math.random() * options.length)];
+    return randomChoice;
+}
 
 // Assign the return value from getComputerChoice to computerSelection variable. 
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection)
+
 
 // Ask the user to input either 'Rock', 'Paper' or 'Scissors'. Store the value in a playerSelection variable. 
 
+
 // Standardise player input to ensure that its case-insensitive. 
+
+
 
 // create playRound function that has two parameters(computerSelection and playerSelection). 
     // compare the values to decide the outcome of the game. 
@@ -17,7 +29,7 @@
     // create keepGoing variable and initialise to True
 
 
-    
+
     // While the keepGoing variable is true call the playRound function
     // depending on the outcome of the game increment either the playerScore or computerScore variable by 1.
     // After each round decrement the roundCount variable by 1. Once the variable reaches 0 reassign the keepGoing variable to False. 
