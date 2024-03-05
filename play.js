b7974fa7ec23f9beb8d9a62e2aa886b70b28c798
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-03-05 14:19:28 +0000

    Convert game from java console to UI.
    Added three buttons to correspond with the options
    in Rock, Paper, Scissors. Added eventlisteners to each of these buttons.
    As such, once clicked, the playRound function is called.

--- play.js
+++ play.js
@@ -7,11 +7,6 @@ function getComputerChoice() {
 
 
 
-
-
-
-
-
 // Standardise player input to ensure that its case-insensitive. 
 function capitalise() {
     let playerInput = prompt("Input your choice here: ");
@@ -39,60 +34,19 @@ function playRound(computerSelection, playerSelection) {
     return 2;
 }
 
+// add event listeners that calls playRound function when clicked.
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', playRound())
+
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', playRound())
 
 
 
-// create playGame function. 
-function playGame() {
-    // create playerScore variable and initialise to 0
-    let playerScore = 0;
-
-    // create computerScore variable and initialise to 0
-    let computerScore = 0;
-
-    // create roundCount variable and initialise to 5
-    let roundCount = 5;
-
-    // create keepGoing variable and initialise to True
-    let keepGoing = true;
-
-    // While the keepGoing variable is true call the playRound function
-    while (keepGoing) {
-        // Assign the return value from getComputerChoice to computerSelection variable. 
-        let computerSelection = getComputerChoice()
-
-        // Ask the user to input either 'Rock', 'Paper' or 'Scissors'. Store the value in a playerSelection variable. 
-        let playerSelection = capitalise()
-
-        let round = playRound(computerSelection, playerSelection)
-        roundCount -= 1;
-        
-        if (round == 1) {
-            console.log(`You Win ${playerSelection} beats ${computerSelection}!`)
-            playerScore += 1;
-        } else if (round == 2) {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
-            computerScore += 1;
-        } else {
-            console.log("Tie!")
-            playerScore += 1;
-            computerScore += 1;
-        }
-
-        if (roundCount == 0 && playerScore > computerScore) {
-            console.log("You Win!")
-            keepGoing = false;
-        } else if (roundCount == 0 && computerScore > playerScore) {
-            console.log("You Lose!")
-            keepGoing = false;
-        }
-    }
-}
-
-playGame()
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', playRound())
 
 
 
-    // depending on the outcome of the game increment either the playerScore or computerScore variable by 1.
-    // After each round decrement the roundCount variable by 1. Once the variable reaches 0 reassign the keepGoing variable to False. 
-    // compare playerScore and computerScore and output the winner to the console. 
+
