629ffee12c41e585f46aaeb8b769999fce4d9107
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-03-12 18:27:49 +0000

    Edit and display game score

--- play.js
+++ play.js
@@ -6,46 +6,58 @@ function getComputerChoice() {
 }
 
 
-
-// Standardise player input to ensure that its case-insensitive. 
-function capitalise() {
-    let playerInput = prompt("Input your choice here: ");
-    let firstLetter = playerInput[0].toUpperCase();
-    let letters = playerInput.slice(1).toLowerCase();
-    return firstLetter + letters;
-}
-
+const result = document.querySelector('#result');
+let playerScore = 0;
+let computerScore = 0;
 
 
 
 // create playRound function that has two parameters(computerSelection and playerSelection). 
 function playRound(computerSelection, playerSelection) {
     // compare the values to decide the outcome of the game. 
+    
     if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        // Return a string that declares the outcome of the game.
-        return 1;
+        playerScore += 1;
+        result.textContent = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
     } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return 1;
+        playerScore += 1;
+        result.textContent = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
     } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return 1;
+        playerScore += 1;
+        result.textContent = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
     } else if (playerSelection === computerSelection) {
-        return 0;
+        result.textContent = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+    } else {
+        computerScore += 1;
+        result.textContent = `Your Score: ${playerScore}, Computer Score: ${computerScore}`;
     }
-    return 2;
+
+    if (playerScore == 5) {
+        result.textContent = "You Have Won!";
+    } else if (computerScore == 5) {
+        result.textContent = "The Computer Has Won!";
+    }
+    
 }
+
+
 
 // add event listeners that calls playRound function when clicked.
 const rockBtn = document.querySelector('#rock');
-rockBtn.addEventListener('click', playRound())
+rockBtn.addEventListener('click', () => {
+    playRound(getComputerChoice(), "Rock");
+});
 
 
 const paperBtn = document.querySelector('#paper');
-paperBtn.addEventListener('click', playRound())
-
-
+paperBtn.addEventListener('click', () => {
+    playRound(getComputerChoice(), "Paper");
+});
 
 const scissorsBtn = document.querySelector('#scissors');
-scissorsBtn.addEventListener('click', playRound())
+scissorsBtn.addEventListener('click', () => {
+    playRound(getComputerChoice(), "Scissors");
+})
 
 
 
